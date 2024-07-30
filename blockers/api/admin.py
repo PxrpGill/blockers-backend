31ea69501d192f51d030ae8b6e4388fe943fbad8
Eugene <jhongil3007@gmail.com>
2024-07-30 18:43:57 +0300

    Update: Изменение моделей БД

--- blockers/api/admin.py
+++ blockers/api/admin.py
@@ -6,85 +6,130 @@ from . import models
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "email",
-        "full_name", "is_active",
-        "is_superuser", "created_at",
-        "updated_at"
+        "id",
+        "email",
+        "full_name",
+        "team",
+        "role",
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
     )
-    
 
-@admin.register(models.UserToken)
-class UserTokenAdmin(admin.ModelAdmin):
-    list_display = (
-        "id", "user",
-        "token", "expired_at",
-        "created_at", "updated_at"
-    )
-    
 
 @admin.register(models.Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "name",
-        "image", "slug",
-        "release_at", "created_by",
-        "created_at", "updated_at"
-    )
-    
-
-@admin.register(models.ProjectRelease)
-class ProjectReleaseAdmin(admin.ModelAdmin):
-    list_display = (
-        "id", "project",
-        "name", "release_at",
-        "created_by", "created_at",
+        "id",
+        "status",
+        "name",
+        "jira_id",
+        "release",
+        "released_at",
+        "created_by",
+        "updated_by",
+        "created_at",
         "updated_at",
     )
-    
+
 
 @admin.register(models.ProjectSection)
 class ProjectSectionAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "project", 
-        "name", "created_by",
+        "id",
+        "project",
+        "name",
+        "created_by",
+        "updated_by",
         "created_at",
-        "updated_at"
+        "updated_at",
     )
-    
-    
+
+
+@admin.register(models.ProjectRelease)
+class ProjectReleaseAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "released_at",
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
+    )
+
+
 @admin.register(models.Status)
 class StatusAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "name", "color",
-        "created_by", "created_at",
-        "updated_at", "deleted_at"
+        "id",
+        "name",
+        "color",
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
     )
-    
-    
-@admin.register(models.ProjectStatus)
-class ProjectStatusAdmin(admin.ModelAdmin):
-    list_display = (
-        "id", "project",
-        "status", "created_at",
-        "updated_at"
-    )
-    
+
 
 @admin.register(models.ProjectTask)
-class ProjecTaskAdmin(admin.ModelAdmin):
+class ProjectTask(admin.ModelAdmin):
     list_display = (
-        "id", "section", "status",
+        "id",
+        "is_active",
+        "section",
+        "release",
         "name",
-        "start_at", "end_at",
-        "created_by", "created_at",
-        "updated_at"
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
+        "deleted_at",
     )
-    
+
 
 @admin.register(models.ProjectTaskEvent)
 class ProjectTaskEventAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "task",
-        "status", "created_by",
-        "created_at", "updated_at"
+        "id",
+        "task",
+        "status",
+        "responsible",
+        "risk",
+        "type",
+        "started_at",
+        "ended_at",
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    )
+
+
+@admin.register(models.UserProject)
+class UserProjectAdmin(admin.ModelAdmin):
+    list_display = ("user", "project")
+
+
+@admin.register(models.UserToken)
+class UserTokenAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "token",
+        "expired_at",
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(models.ProjectStatus)
+class ProjectStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        "project",
+        "status",
     )
--- blockers/api/models.py
+++ blockers/api/models.py
@@ -4,70 +4,58 @@ import uuid
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(max_length=320, unique=True, null=True, blank=True)
     full_name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
+    team = models.CharField(max_length=32, help_text="Команда пользователя")
+    role = models.CharField(max_length=32, default="role_user")
+    password = models.CharField(max_length=255, null=True, blank=True)
+    created_by = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        related_name="user_created_by",
+        on_delete=models.SET_NULL,
+    )
+    updated_by = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        related_name="user_updated_by",
+        on_delete=models.SET_NULL,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.full_name
 
 
-class UserToken(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255)
-    expired_at = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Токен пользователя"
-        verbose_name_plural = "Токены пользователей"
-
-    def __str__(self) -> str:
-        return self.token
-
-
 class Project(models.Model):
+    STATUS_CHOICES = [
+        ("new", "New"),
+        ("develop", "Develop"),
+        ("released", "Released"),
+    ]
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status = models.CharField(max_length=32, choices=STATUS_CHOICES)
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
-    release_at = models.DateField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    jira_id = models.CharField(max_length=32, null=True, blank=True)
+    slug = models.SlugField(max_length=32, unique=True)
+    release = models.ForeignKey(
+        "ProjectRelease", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    released_at = models.DateTimeField()
+    created_by = models.ForeignKey(
+        User, related_name="project_created_by", on_delete=models.CASCADE
+    )
+    updated_by = models.ForeignKey(
+        User, related_name="project_updated_by", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        verbose_name = "Проект"
-        verbose_name_plural = "Проекты"
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class ProjectRelease(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    release_at = models.DateField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Релиз проекта"
-        verbose_name_plural = "Релизы проекта"
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
 
@@ -75,72 +63,151 @@ class ProjectSection(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        User, related_name="project_section_created_by", on_delete=models.CASCADE
+    )
+    updated_by = models.ForeignKey(
+        User, related_name="project_section_updated_by", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        verbose_name = "Секция проекта"
-        verbose_name_plural = "Секции проекта"
+    def __str__(self):
+        return self.name
 
-    def __str__(self) -> str:
+
+class ProjectRelease(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    comment = models.CharField(max_length=512, null=True, blank=True)
+    released_at = models.DateTimeField()
+    created_by = models.ForeignKey(
+        User, related_name="project_release_created_by", on_delete=models.CASCADE
+    )
+    updated_by = models.ForeignKey(
+        User, related_name="project_release_updated_by", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
         return self.name
 
 
 class Status(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    color = models.CharField(max_length=255)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    color = models.CharField(max_length=12)
+    created_by = models.ForeignKey(
+        User, related_name="status_created_by", on_delete=models.CASCADE
+    )
+    updated_by = models.ForeignKey(
+        User, related_name="status_updated_by", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
 
-    class Meta:
-        verbose_name = "Статус"
-        verbose_name_plural = "Статусы"
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
-
-
-class ProjectStatus(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Статус проекта"
-        verbose_name_plural = "Статусы проекта"
 
 
 class ProjectTask(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_active = models.BooleanField(default=True)
     section = models.ForeignKey(ProjectSection, on_delete=models.CASCADE)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    release = models.ForeignKey(
+        ProjectRelease, null=True, blank=True, on_delete=models.SET_NULL
+    )
     name = models.CharField(max_length=255)
-    сomment = models.TextField()
-    start_at = models.DateTimeField()
-    end_at = models.DateTimeField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(null=True, blank=True)
+    created_by = models.ForeignKey(
+        User, related_name="project_task_created_by", on_delete=models.CASCADE
+    )
+    updated_by = models.ForeignKey(
+        User, related_name="project_task_updated_by", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
-    class Meta:
-        verbose_name = "Таска"
-        verbose_name_plural = "Таски"
+    def __str__(self):
+        return self.name
 
 
 class ProjectTaskEvent(models.Model):
+    RISK_CHOICES = [
+        ("done", "Готово"),
+        ("have_blocker", "Есть блокеры"),
+        ("no_risk", "Нет рисков"),
+    ]
+
+    TYPE_CHOICES = [
+        ("done", "Выполнено"),
+        ("transfer", "Перенос"),
+    ]
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     task = models.ForeignKey(ProjectTask, on_delete=models.CASCADE)
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    responsible = models.ForeignKey(
+        User, related_name="project_task_event_responsible", on_delete=models.CASCADE
+    )
+    risk = models.CharField(max_length=32, choices=RISK_CHOICES)
+    type = models.CharField(max_length=32, choices=TYPE_CHOICES)
+    comment = models.TextField(null=True, blank=True)
+    started_at = models.DateTimeField(null=True, blank=True)
+    ended_at = models.DateTimeField(null=True, blank=True)
+    created_by = models.ForeignKey(
+        User, related_name="project_task_event_created_by", on_delete=models.CASCADE
+    )
+    updated_by = models.ForeignKey(
+        User, related_name="project_task_event_updated_by", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Event for Task {self.task.name}"
+
+
+class UserProject(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "project")
+
+
+class UserToken(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=64)
+    expired_at = models.DateTimeField()
+    created_by = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        related_name="user_token_created_by",
+        on_delete=models.SET_NULL,
+    )
+    updated_by = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        related_name="user_token_updated_by",
+        on_delete=models.SET_NULL,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"Token for User {self.user.email}"
+
+
+class ProjectStatus(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+
     class Meta:
-        verbose_name = "Событие"
-        verbose_name_plural = "События"
+        unique_together = ("project", "status")
